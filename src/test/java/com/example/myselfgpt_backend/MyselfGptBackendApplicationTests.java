71cb01494f0566a0729942eca58ea217c336ac9e
XiChen1234 <xichen888.private@gmail.com>
2024-05-11 20:09:39 +0800

    init: init the project

---
+++ src/test/java/com/example/myselfgpt_backend/MyselfGptBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myselfgpt_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyselfGptBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
